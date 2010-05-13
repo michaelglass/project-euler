c9dc462b0233c875617b15017b1909f5dfbca30a
Michael Glass <me@mike.is>
2010-05-13 14:20:10 -0400

    cheated on 12 a little ...

--- 12.rb
+++ 12.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require 'time_this'
 require 'prime'
-
+require 'set'
 
 class TriangleGen
   def self.next
@@ -19,42 +19,20 @@ class TriangleGen
   def self.sum
     @@sum
   end
-
 end
 
-def factors_from_prime_factors(factor_arr) #can i do this faster??  MATH!
-  factor_arr = factor_arr.dup
-  factors = [] #1 is considered a factor...
-  prime_factors = factor_arr.length
-  factor_arr = factor_arr.dup
-  
-  factor_arr.each_with_index do |factor, index|
-    factor1, pow1 = factor
-    (1..pow1).each do |power1|
-      factors << factor1**power1
-      ((index+1)...factor_arr.length).each do |ind|
-        factor2, pow2 = factor_arr[ind]
-        (1..pow2).each do |power2|
-          factors << factor1**power1 * factor2**power2
-        end
-      end
-    end
+def num_divisors(num)
+  divisors = 1
+  Prime.prime_division(num).each do |num, pow|
+    divisors *= pow+1
   end
-  factors.sort
+  divisors
 end
 
-def factors(num)
-  factor_arr = Prime.prime_division num
-  factors_from_prime_factors(factor_arr)
+
+while((divisors = num_divisors(TriangleGen.next)) < 500)
 end
 
-# num_divisors = -1
-# while num_divisors < 500
-#   # exit if(Time.now - START_TIME > 60)
-#   (rand*1000).to_i.times {TriangleGen.next}
-#   num_factors = factors(TriangleGen.next).length
-#   if num_factors > num_divisors
-#     num_divisors = num_factors 
-#     puts "#{TriangleGen.current}th (#{TriangleGen.sum}) => #{num_factors}"
-#   end
-# end
+puts [divisors, TriangleGen.current, TriangleGen.sum].inspect
+
+
